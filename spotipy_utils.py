8b693c2cf7c40c86dcb5bfed8ea5d13486e7da35
Darren Schachter <dts86@cornell.edu>
2020-06-03 12:09:31 -0400

    Add Song class for storing metadata

--- spotipy_utils.py
+++ spotipy_utils.py
@@ -1,6 +1,31 @@
 import spotipy
 import spotipy.util as util
 
+class Song:
+    def __init__(self, track_id, sp):
+        self.track_id = track_id
+        self.sp = sp
+
+        self.pull_song_data()
+
+    def pull_song_data(self):
+        track = self.sp.track(self.track_id)
+
+        self.title = track['name']
+        self.album = track['album']['name']
+
+        self.artists = list()
+        for artist in track['artists']:
+            self.artists.append(artist['name'])
+
+    def __str__(self):
+        out = "Title: " + str(self.title) + '\n'
+        out += ("Album: " + str(self.album) + '\n')
+        out += "Aritsts:\n"
+        for artist in self.artists:
+            out += (" * " + artist + "\n")
+        return out
+
 def authenticate_user():
     CLIENT_ID = input("CLIENT_KEY: ")
     CLIENT_SECRET = input("CLIENT_SECRET: ")
@@ -19,6 +44,6 @@ def authenticate_user():
 
     return token
 
-def get_current_track(sp):
+def get_current_track_id(sp):
     curr = sp.current_playback()
     return curr
