91ac651d7d688d3da6899c3ea49b4a724bf44a76
Darren Schachter <dts86@cornell.edu>
2020-06-03 15:44:11 -0400

    Add SongFeatures class

--- spotipy_utils.py
+++ spotipy_utils.py
@@ -1,6 +1,37 @@
 import spotipy
 import spotipy.util as util
 
+class SongFeatures:
+    def __init__(self, track_id=None, sp=None):
+        if track_id != None and sp != None:
+            self.populate_features_by_track(track_id, sp)
+            return
+            
+        self.acousticness = None
+        self.danceability = None
+        self.energy = None
+        self.instrumentalness = None
+        self.liveness = None
+        self.loudness = None
+        self.speechiness = None
+        self.tempo = None
+        self.valence = None
+
+    def populate_features_by_track(self, track_id, sp):
+        if track_id == None or sp == None:
+            return
+
+        song_attrs = sp.audio_features([track_id])[0]       
+        self.acousticness = song_attrs['acousticness']
+        self.danceability = song_attrs['danceability']
+        self.energy = song_attrs['energy']
+        self.instrumentalness = song_attrs['instrumentalness']
+        self.liveness = song_attrs['liveness']
+        self.loudness = song_attrs['loudness']
+        self.speechiness = song_attrs['speechiness']
+        self.tempo = song_attrs['tempo']
+        self.valence = song_attrs['valence']
+
 class Song:
     def __init__(self, track_id, sp):
         self.track_id = track_id
@@ -18,16 +49,7 @@ class Song:
         for artist in track['artists']:
             self.artists.append(artist['name'])
 
-        song_attrs = self.sp.audio_features([self.track_id])[0]       
-        self.acousticness = song_attrs['acousticness']
-        self.danceability = song_attrs['danceability']
-        self.energy = song_attrs['energy']
-        self.instrumentalness = song_attrs['instrumentalness']
-        self.liveness = song_attrs['liveness']
-        self.loudness = song_attrs['loudness']
-        self.speechiness = song_attrs['speechiness']
-        self.tempo = song_attrs['tempo']
-        self.valence = song_attrs['valence']
+        self.sf = SongFeatures(track_id=self.track_id,sp=self.sp)
 
         print()
 
@@ -38,15 +60,15 @@ class Song:
         for artist in self.artists:
             print(" * ", artist)
         print("-------------------------------------")
-        print("Acousticness: ", self.acousticness)
-        print("Danceability: ", self.danceability)
-        print("Energy: ", self.energy)
-        print("Instrumentalness: ", self.instrumentalness)
-        print("Liveness: ", self.liveness)
-        print("Loudness: ", self.loudness)
-        print("Speechiness: ", self.speechiness)
-        print("Tempo: ", self.tempo)
-        print("Valence: ", self.valence)
+        print("Acousticness: ", self.sf.acousticness)
+        print("Danceability: ", self.sf.danceability)
+        print("Energy: ", self.sf.energy)
+        print("Instrumentalness: ", self.sf.instrumentalness)
+        print("Liveness: ", self.sf.liveness)
+        print("Loudness: ", self.sf.loudness)
+        print("Speechiness: ", self.sf.speechiness)
+        print("Tempo: ", self.sf.tempo)
+        print("Valence: ", self.sf.valence)
         
     def __str__(self):
         out = "Title: " + str(self.title) + '\n'
