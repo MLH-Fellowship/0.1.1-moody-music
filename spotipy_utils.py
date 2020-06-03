68a9687d49988829de33dd7860fb671fbb6f1ef5
Darren Schachter <dts86@cornell.edu>
2020-06-03 11:33:42 -0400

    Move authentication code to function

--- spotipy_utils.py
+++ spotipy_utils.py
@@ -1,17 +1,22 @@
 import spotipy
 import spotipy.util as util
 
-CLIENT_ID = input("CLIENT_KEY: ")
-CLIENT_SECRET = input("CLIENT_SECRET: ")
-REDIRECT_URI = 'http://localhost:8080'
+def authenticate_user():
+    CLIENT_ID = input("CLIENT_KEY: ")
+    CLIENT_SECRET = input("CLIENT_SECRET: ")
+    REDIRECT_URI = 'http://localhost:8080'
 
-scope = ''
+    scope = ''
 
-username = input("Enter your Spotify username: ")
+    username = input("Enter your Spotify username: ")
 
-token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-if token:
-    print("User authenticated")
-else:
-    print("User not authenticated")
+    if token:
+        print("User authenticated")
+    else:
+        print("User not authenticated")
+
+    return token
+
+authenticate_user()
