5cd5c062337491b8ceab8cd08ace0e28810dd9c0
Darren Schachter <dts86@cornell.edu>
2020-06-04 19:14:48 -0400

    Fix parentheses / standardize variable names in mood.py

--- mood.py
+++ mood.py
@@ -1,42 +1,41 @@
-class MoodEquations
-#designed to return metrics for reccomendations from percentage of face emotions
+# Designed to return metrics for reccomendations from percentage of face emotions
 
-def get_valence(Angry,Disguist,Fear,Happiness,Sad,Suprise,Neutral): #float betwwen 0 and 1
+def get_valence(Anger,Disgust,Fear,Happy,Sad,Surprise,Neutral): # float between 0 and 1
 	
-	if( ((Sad + Disguist + Fear + Neutral)/4) > (Angry + Happiness + Neutral)/3)) :
-		if ( (((Sad + Disguist + Fear + Neutral)/4) * 1.6666) > 1) #just in case clause
+	if ((Sad + Disgust + Fear + Neutral)/4) > (Anger + Happy + Neutral)/3 :
+		if (Sad + Disgust + Fear + Neutral)/4 * 1.6666 > 1: #just in case clause
 			return 0.001
-		return (1 - (((Sad + Disguist + Fear + Neutral)/4) * 1.6666))
-	else
-		if( (((Angry + Happiness + Neutral)/3)*1.6666) > 1) #just in case clause
+		return 1 - ((Sad + Disgust + Fear + Neutral)/4) * 1.6666
+	else:
+		if ((Anger + Happy + Neutral)/3)*1.666 > 1: #just in case clause
 			return 0.999
-		return ((Angry + Happiness + Neutral)/3)*1.6666
+		return ((Anger + Happy + Neutral)/3)*1.6666
 
-def get_tempo(Anger,Disguist,Fear,Happy,Sad,Suprise,Neutral): #any number ranging from 0-185
+def get_tempo(Anger,Disgust,Fear,Happy,Sad,Surprise,Neutral): #any number ranging from 0-185
 	base_tempo = 75
-	if( ((Sad + Disguist + Fear + Neutral)/4) > (Angry + Happiness + Neutral)/3)) :
-		return (base_tempo - ((Sad + Disguist + Fear + Neutral)/4)/0.03)
-	else
-		return (base_tempo + ((Angry + Happiness + Neutral)/0.00521))
+	if((Sad + Disgust + Fear + Neutral)/4) > (Anger + Happy + Neutral)/3:
+		return base_tempo - ((Sad + Disgust + Fear + Neutral)/4)/0.03
+	else:
+		return (base_tempo + ((Anger + Happy + Neutral)/0.00521))
 
-def get_energy(Anger,Disguist,Fear,Happy,Sad,Suprise,Neutral): # float between 0 and 1
+def get_energy(Anger,Disgust,Fear,Happy,Sad,Surprise,Neutral): # float between 0 and 1
 
-	if( ((Sad + Disguist + Neutral/ 3) > (Angry + Happiness + Neutral)/3)) :
-		if( (((Sad + Disguist + Fear + Neutral)/4) * 1.6666) > 1) #just in case clause
+	if (Sad + Disgust + Neutral)/3 > (Anger + Happy + Neutral)/3:
+		if ((Sad + Disgust + Fear + Neutral)/4) * 1.6666 > 1: #just in case clause
 			return 0.001
-		return (1 - (((Sad + Disguist + Fear + Neutral)/4) * 1.6666))
-	else
-		if ( (((Angry + Happiness + Fear)/3) * 1.6666 ) > 1) #just in case clause
+		return 1 - ((Sad + Disgust + Fear + Neutral)/4) * 1.6666
+	else:
+		if ((Anger + Happy + Fear)/3) * 1.6666 > 1: #just in case clause
 			return 0.999
-		return ((Angry + Happiness + Fear)/3) * 1.6666  
+		return ((Anger + Happy + Fear)/3) * 1.6666  
 
-def get_dancebility(Anger,Disguist,Fear,Happy,Sad,Suprise,Neutral): #float betwwen 0 and 1
-	if( ((Sad + Disguist + Fear + Neutral)/4) > (Angry + Happiness)/2)) :
-		if ( (((Sad + Disguist + Fear + Neutral)/4) * 1.6666) > 1) #just in case clause
+def get_dancebility(Anger,Disgust,Fear,Happy,Sad,Surprise,Neutral): #float betwwen 0 and 1
+	if ((Sad + Disgust + Fear + Neutral)/4) > (Anger + Happy)/2:
+		if ((Sad + Disgust + Fear + Neutral)/4) * 1.6666 > 1: #just in case clause
 			return 0.001
-		return (1 - (((Sad + Disguist + Fear + Neutral)/4) * 1.6666))
-	else
-		if( (((Angry + Happiness)/2)*1.6666) > 1 )#just in case clause
+		return 1 - ((Sad + Disgust + Fear + Neutral)/4) * 1.6666
+	else:
+		if ((Anger + Happy)/2)*1.6666 > 1: #just in case clause
 		   return 0.999
-		return ((Angry + Happiness)/2)*1.6666 
+		return ((Anger + Happy)/2)*1.6666 
 	
