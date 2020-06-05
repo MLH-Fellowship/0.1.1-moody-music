2efc1a66d3f94517acb16440630f068e51adb896
Darren Schachter <dts86@cornell.edu>
2020-06-04 20:55:44 -0400

    Add feature to detect if track has changed

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ NUM_SAMPLES = 10 # number of samples for avg emotion
 SAMPLE_PERIOD = 1 # in seconds
 
 prev_sample_time = 0
+last_track = None
 
 
 # Main Code
@@ -36,14 +37,20 @@ sp = spotipy.Spotify(auth=token)
 
 mood = md.Mood(NUM_SAMPLES)
 while True:
+    # Wait for sample period
     while time.time() - prev_sample_time < SAMPLE_PERIOD:
         pass
 
+    # Record emotions and update mood
     prev_sample_time = time.time()
     emotions = get_emotions()
     mood.add_data_point(emotions)
 
-    print(mood.get_song_features())
+    # Check if song has changed
+    curr_track = get_current_song(sp)
+    if (last_track == None and curr_track != None) or (last_track != None and curr_track != None and last_track.track_id != curr_track.track_id):
+        print("New Song!")
+        last_track = curr_track
 
 sf = emotions_to_song_features(get_emotions())
 
